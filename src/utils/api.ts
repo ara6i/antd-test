75213ac62a3fc3a463cf12685a6b17c8b1c5e391
A Team <ara6i.sn@gmail.com>
2025-03-10 14:37:13 +0300

    cleanup api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://your-backend.com/api/data"; // Replace with your actual backend URL
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
 
 export const sendDataToBackend = async (data: object) => {
   try {
